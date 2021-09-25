3acc4a2bf7f892946dfa3e6f7198bf7b6d4e0523
njbultman <njbultman74@gmail.com>
2021-09-25 10:15:16 -0500

    Fixing unit test.

--- tests/testthat/test-get_user_data.R
+++ tests/testthat/test-get_user_data.R
@@ -1,3 +1,3 @@
-test_that("returns a data frame", {
-  expect_equal(class(get_user_data(688556535013502976)), "data.frame")
+test_that("returns a list", {
+  expect_equal(class(get_user_data(688556535013502976)), "list")
 })
