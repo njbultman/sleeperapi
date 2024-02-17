761c278901db615d03fbbc2c1614aacfd6906dbc
njbultman <njbultman74@gmail.com>
2024-02-17 15:18:49 -0600

    Removing unit tests on get_avatar_picture to avoid R CMD Check errors on Windows.

--- tests/testthat/test-get_avatar_picture.R
+++ tests/testthat/test-get_avatar_picture.R
@@ -5,13 +5,3 @@ test_that("Returns error if invalid type specified", {
   expect_error(get_avatar_picture("c751b27d9158c1dd41bd33dc7e4bcdde", type = "invalid", keep_image = FALSE), "Invalid value entered for type: can only be 'full' or 'thumbnail'")
 })
 
-# Test 2: Informs user that file was deleted if keep_image is FALSE
-test_that("Returns error if invalid type specified", {
-  expect_message(get_avatar_picture("c751b27d9158c1dd41bd33dc7e4bcdde", type = "full", keep_image = FALSE), "File successfully removed from temporary location.")
-})
-
-# Test 3: Informs user that file was not deleted if keep_image is TRUE
-test_that("Returns error if invalid type specified", {
-  expect_message(get_avatar_picture("c751b27d9158c1dd41bd33dc7e4bcdde", type = "thumbnail", keep_image = TRUE), regexp = "Temporary file not deleted. It can be found at*")
-})
-
